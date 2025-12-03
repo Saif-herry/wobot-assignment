49bcc6ca0a7a41f8992a09ffcdf95cd34ad3ef4e
Mohammad Saif <saifsami321md@gmail.com>
2025-12-04 00:08:10 +0530

    minor changes

--- src/components/TableComponent.jsx
+++ src/components/TableComponent.jsx
@@ -17,7 +17,6 @@ function TableComponent() {
           <tr>
             <th>Column 1</th>
             <th>Column 2</th>
-            <th>Column 3</th>
           </tr>
         </thead>
         <tbody>
