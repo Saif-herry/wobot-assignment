0acbbdf022e960cd936de54c3d56486da102c2ca
Mohammad Saif <saif@Mohammads-MacBook-Air.local>
2025-12-03 23:45:38 +0530

    update

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import TableComponent from "./components/TableComponent";
+
+function App() {
+  return (
+    <>
+      <TableComponent />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/TableComponent.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Table.css";
+
+function TableComponent() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1>Hello Table</h1>
+    </div>
+  );
+}
+
+export default TableComponent;
