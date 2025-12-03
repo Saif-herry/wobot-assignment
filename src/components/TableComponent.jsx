f8f5f310540f8f68a258bc701edfd05c40c52538
Mohammad Saif <saif@Mohammads-MacBook-Air.local>
2025-12-03 23:59:53 +0530

    update

--- src/components/TableComponent.jsx
+++ src/components/TableComponent.jsx
@@ -12,6 +12,22 @@ function TableComponent() {
       }}
     >
       <h1>Hello Table</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Column 1</th>
+            <th>Column 2</th>
+            <th>Column 3</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Data 1</td>
+            <td>Data 2</td>
+            <td>Data 3</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
